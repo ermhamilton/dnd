265c9b8dee061da195d10af624488c17ab807012
Erin Hamilton <ehamilton@kaboom.org>
2016-06-30 17:09:47 -0400

    Add rails files

---
+++ config/initializers/spina.rb
@@ -0,0 +1,39 @@
+Spina.configure do |config|
+  config.NEGATIVE_CAPTCHA_SECRET = '2f8ba910f8b5654b0c7b28049cf348af5099caf988a5b1b292617e7edbae811d2b6ed232a832ff0790783e46d95ab383fbd261c95d1f589faf7c80d785428d60'
+
+  # Important Note
+  # ==============
+
+  # You MUST restart your server before changes to this file
+  # will take effect.
+
+  # Specify a backend path. Defaults to /admin.
+  # config.backend_path = 'admin'
+
+  # Pages Options
+  # ===============
+
+  # Note that you might need to remove cached asset after changing this value
+  # config.max_page_depth = 5
+
+  # Storage Options
+  # ===============
+
+  # Please specify how you want to store photos, your logo, and
+  # other files. We use CarrierWave for storage. See
+  # https://github.com/denkGroot/Spina/tree/master/app/uploaders/spina
+
+  # config.storage = :file
+
+  # If you want to use s3 to store uploads (recommended)
+
+  # config.storage = :s3
+  # config.aws_region = "eu-west-1"
+  # config.aws_access_key_id = "abc123"
+  # config.aws_secret_key = "abc123"
+  # config.s3_bucket = "mybucket"
+  # If you want to store your files localy (not recommended for
+  # production, in large part because it's more difficult to ensure
+  # that files are backed up in sync with your database):
+
+end
